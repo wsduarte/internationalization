bf5a5c6b8703c72b93177c6fada31c3a4ec9eb55
josejbocanegra <josejbocanegra@gmail.com>
2019-03-29 18:53:31 -0500

    Adding bootstrap

--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -5,22 +5,30 @@ export default class JobsList extends React.Component {
 
   state = { 
   	"offers": [
-  	{
-  	  "id": "0001",
-      "name": "Asesor comercial de hipermercado",
-  	  "company": "Schneider Electric", 
-  	  "salary": 4.5,
-  	  "city": "Bogotá, Colombia",
-      "date": "15/02/2019"
-    }, 
-    {
-      "id": "0002",
-      "name": "Desarrollador de software",
-      "company": "Google Inc.", 
-      "salary": 20,
-      "city": "Palo Alto, CA, USA",
-      "date": "23/13/2019"
+    	{
+    	  "id": "0001",
+        "name": "Asesor comercial de hipermercado",
+    	  "company": "Schneider Electric", 
+    	  "salary": 4.5,
+    	  "city": "Bogotá, Colombia",
+        "date": "15/02/2019"
+      }, 
+      {
+        "id": "0002",
+        "name": "Desarrollador de software",
+        "company": "Google Inc.", 
+        "salary": 20,
+        "city": "Palo Alto, CA, USA",
+        "date": "23/03/2019"
       },
+      {
+        "id": "0003",
+        "name": "Enfermera",
+        "company": "Clínica La Aurora", 
+        "salary": 1,
+        "city": "Cali, Colombia",
+        "date": "23/03/2019"
+      }
     ]
   };
 
